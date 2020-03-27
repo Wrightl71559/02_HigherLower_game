3aa47cdc57870f16f31d02db47d55d160ccce01c
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-03-27 18:52:01 +1300

    trsjtrj

---
+++ 09a_stats_experiment.py
@@ -0,0 +1,26 @@
+# component 9 - part 1
+
+# figure out game stats
+# get game stats to display nicely
+
+game_stats =[3, 1, 6, 3, 4]
+
+# print game outcomes
+print("*** Score for each round.. ***")
+list_count = 1
+for item in game_stats:
+    print("Round{}: {}".format(list_count, item))
+    list_count += 1
+
+# Calculate statistics
+game_stats.sort()
+best = game_stats[0]
+worst = game_stats[-1]
+average = sum(game_stats)/len(game_stats)
+
+print()
+print("*** Summary Statistics ***")
+print("Best: {}".format(best))
+print("Worst: {}".format(worst))
+print("Average: {}".format(average))
+
